24a96b8f650ef17b48bb9ba150108e68d5e02802
Yakubov Yakub <nodirayakubova26@gmail.com>
2021-12-09 11:03:24 +0500

    some

--- models/Product.js
+++ models/Product.js
@@ -4,7 +4,7 @@ const ProductSchema = new mongoose.Schema(
   {
     title: { type: String, required: true, unique: true },
     img: { type: String, required: true },
-    price: { type: Number, required: true },
+    price: { type: String, required: true },
     type: { type: String, required: true },
   },
   { timestamps: true }
